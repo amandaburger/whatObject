1ede17fd3f9791449ff6d9a52ecba8c5906e37e6
Amanda Burger <amandaburger01@gmail.com>
2020-05-25 00:20:45 -0400

    Initial Commit

---
+++ whatObject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatObject
+//
+//  Created by Amanda Burger on 5/25/20.
+//  Copyright © 2020 Amanda Burger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
